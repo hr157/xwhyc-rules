605ff2368598aa6dffc50b878675646f80328e24
hr157 <hr157@sina.com>
2020-06-17 16:54:55 +0800

    Add files via upload

--- update3.js
+++ update3.js
@@ -1,6 +1,4 @@
-;更新规则url,dir,size,reverse,reverse
-https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt	\data\lazy.txt	559336	4	0	
-https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt	\data\video.txt	 1128	4	0	
+;update rules url,dir,size,reverse,reverse
+https://raw.githubusercontent.com/hr157/xwhyc-rules/master/lazy.txt	\data\lazy.txt	559383	4	0	
+https://raw.githubusercontent.com/hr157/xwhyc-rules/master/video.txt	\data\video.txt	 1151	4	0	
 !http://update.adbyby.com/rule3/user.action	\user.action	4	5	0	
-
-
